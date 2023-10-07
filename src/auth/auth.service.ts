e77c4e65ed05ef42fe590a60f75b2052f7b3c317
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-10-07 11:01:22 -0300

    User registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
 import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class AuthService {
@@ -15,14 +16,26 @@ export class AuthService {
         // generate the password hash
         const hash = await argon.hash(dto.password);
         // save the new user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
-            },
-        });
-        delete user.hash
-        // return the saved user
-        return user;
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+            delete user.hash
+
+            // return the saved user
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException(
+                        'Credentials taken',
+                    );
+                }
+            }
+            throw error
+        }
     }
 }
